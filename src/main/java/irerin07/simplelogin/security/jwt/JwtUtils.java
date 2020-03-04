064982f29fe311624391a2cc8b98b9529a11ff6d
irerin07 <counterchord89@gmail.com>
2020-03-04 23:24:52 +0900

    jwt

---
+++ src/main/java/irerin07/simplelogin/security/jwt/JwtUtils.java
@@ -0,0 +1,10 @@
+package irerin07.simplelogin.security.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+}

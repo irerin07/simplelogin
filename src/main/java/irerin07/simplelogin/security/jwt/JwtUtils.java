9720b6b2a3b9551d44e131c0d18c5e16719959f5
irerin07 <counterchord89@gmail.com>
2020-03-06 01:21:36 +0900

    jwt

--- src/main/java/irerin07/simplelogin/security/jwt/JwtUtils.java
+++ src/main/java/irerin07/simplelogin/security/jwt/JwtUtils.java
@@ -1,10 +1,36 @@
 package irerin07.simplelogin.security.jwt;
 
+import irerin07.simplelogin.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import io.jsonwebtoken.*;
 
 @Component
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${my.app.jwtSecret}")
+    private String jwtSecret;
+    @Value("${my.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        User user = (User) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((user.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+
 }
